b64ecb363979f0471c0e78fe86ce204dd2fa8155
susantshrestha <susantshrestha@hotmail.com>
2016-12-06 15:55:35 +0545

    first

---
+++ Webproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webproject.Startup))]
+namespace Webproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
